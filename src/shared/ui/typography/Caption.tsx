0541461727504eab5251eeb6bbaad7eff1d4d88d
Kopite22 <nulee1000@gmail.com>
2025-09-09 17:27:47 +0900

    💄 - typography 추가

---
+++ src/shared/ui/typography/Caption.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const Caption1 = styled.p`
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.0252em;
+  font-weight: 700;
+`;
+
+const Caption2 = styled.p`
+  font-size: 11px;
+  line-height: 14px;
+  letter-spacing: 0.0311em;
+`;
+
+export { Caption1, Caption2 };
---
+++ src/shared/ui/typography/inext.ts
@@ -0,0 +1,5 @@
+export * from "./Title";
+export * from "./Heading";
+export * from "./Headline";
+export * from "./Body"; 
+export * from "./Label";

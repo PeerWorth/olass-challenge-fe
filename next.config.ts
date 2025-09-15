ec851ffcb1bba089218d96d83d1053a1d0c22961
Kopite22 <nulee1000@gmail.com>
2025-09-15 17:19:28 +0900

    📦 - svgr/webpack 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,24 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   async rewrites() {
     return [
       {

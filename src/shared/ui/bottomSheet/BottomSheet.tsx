12627301b947042d3924b0e6678a6c80264ebc19
Kopite22 <nulee1000@gmail.com>
2025-09-16 17:08:34 +0900

    💄 - 상단 바 추가

--- src/shared/ui/bottomSheet/BottomSheet.tsx
+++ src/shared/ui/bottomSheet/BottomSheet.tsx
@@ -9,7 +9,8 @@ const BottomSheet = ({ children }: { children: React.ReactNode }) => {
       className="flex items-end justify-center px-5 pb-5"
       onClose={handleClose}
     >
-      <div className={cn("bg-common100 w-full rounded-3xl p-5")}>
+      <div className={cn("bg-common100 w-full rounded-3xl px-5 pt-2 pb-5")}>
+        <div className="mx-auto h-[5px] w-10 rounded-full bg-[#70737C29]" />
         {children}
       </div>
     </PortalOverlay>

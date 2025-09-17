62726527f537a5c970775f72f04f4622b13e8685
Kopite22 <nulee1000@gmail.com>
2025-09-17 14:33:51 +0900

    💄 - screenVariants 추가

--- src/shared/ui/screen/ScreenVariants.ts
+++ src/shared/ui/screen/ScreenVariants.ts
@@ -3,7 +3,15 @@ import { cva } from "class-variance-authority";
 const ScreenVariants = cva(["overflow-auto"], {
   variants: {
     deviceType: {
-      desktop: ["w-[375px]", "h-[812px]", "fixed", "inset-0", "m-auto"],
+      desktop: [
+        "w-[375px]",
+        "h-[812px]",
+        "fixed",
+        "inset-0",
+        "m-auto",
+        "border",
+        "border-black",
+      ],
       mobile: ["w-dvw", "h-dvh"],
     },
   },

201af2924180fd5dcf77c53834af3f18c5ab81a1
Kopite22 <nulee1000@gmail.com>
2025-09-17 14:00:31 +0900

    ✨ - 스크린 레이아웃 추가

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,10 +1,14 @@
+import { Screen } from "@/shared/ui/screen";
+
 import ModalProvider from "./ModalProvider";
 import QueryProvider from "./QueryProvider";
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryProvider>
-      <ModalProvider>{children}</ModalProvider>
+      <Screen>
+        <ModalProvider>{children}</ModalProvider>
+      </Screen>
     </QueryProvider>
   );
 };

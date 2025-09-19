ffac560e36787f7dcb1ad230cf1bef81e1e516ec
Kopite22 <nulee1000@gmail.com>
2025-09-19 15:46:29 +0900

    ✨ - 랜딩페이지 추가

--- src/views/landing/ui/LandingPage.tsx
+++ src/views/landing/ui/LandingPage.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import Image from "next/image";
+
+import { useBottomSheet } from "@/shared/hooks";
 import { Button } from "@/shared/ui/button";
 
 import { LANDING_SECTION_CONTENT } from "../model/constants";
@@ -5,36 +10,61 @@ import ChallengeCard from "./ChallengeCard";
 import SectionLayout from "./SectionLayout";
 import Title from "./Title";
 import Toast from "./Toast";
+import ImageMobile from "./images/mobile-image.png";
+import ImageSavedMoney from "./images/saved-money.png";
 
 const LandingPage = () => {
+  const { handleOpen } = useBottomSheet();
+  const handleClick = () => {
+    handleOpen(<div className="h-50">BottomSheet</div>);
+  };
+
   return (
     <>
-      <div className="pb-10">
-        <div className="flex-1">
-          <Toast />
-
-          <Title />
-          <div className="flex flex-col gap-y-5">
-            <SectionLayout
-              title={LANDING_SECTION_CONTENT.others.title}
-              description={LANDING_SECTION_CONTENT.others.description}
-              content={
-                <div className="scrollbar-hide flex gap-x-3 overflow-x-scroll px-4">
-                  <ChallengeCard />
-                  <ChallengeCard />
-                  <ChallengeCard />
-                </div>
-              }
-            />
-            <SectionLayout
-              title={LANDING_SECTION_CONTENT.together.title}
-              description={LANDING_SECTION_CONTENT.together.description}
-            />
-            <SectionLayout
-              title={LANDING_SECTION_CONTENT.now.title}
-              description={LANDING_SECTION_CONTENT.now.description}
-            />
-          </div>
+      <div className="flex-1 pb-12">
+        <Toast />
+        <Title />
+        <div className="flex flex-col gap-y-5">
+          <SectionLayout
+            title={LANDING_SECTION_CONTENT.others.title}
+            description={LANDING_SECTION_CONTENT.others.description}
+            content={
+              <div className="scrollbar-hide flex gap-x-3 overflow-x-scroll px-4">
+                <ChallengeCard />
+                <ChallengeCard />
+                <ChallengeCard />
+              </div>
+            }
+          />
+          <SectionLayout
+            title={LANDING_SECTION_CONTENT.together.title}
+            description={LANDING_SECTION_CONTENT.together.description}
+            content={
+              <div className="bg-coolNeutral25 mx-4 rounded-3xl">
+                <Image
+                  className="mx-auto"
+                  src={ImageMobile.src}
+                  alt="mobileImg"
+                  width={343}
+                  height={258}
+                />
+              </div>
+            }
+          />
+          <SectionLayout
+            title={LANDING_SECTION_CONTENT.now.title}
+            description={LANDING_SECTION_CONTENT.now.description}
+            content={
+              <div className="mx-4 flex justify-center rounded-3xl bg-[#E7F3FF] py-[18px]">
+                <Image
+                  src={ImageSavedMoney.src}
+                  alt="savedMoneyImg"
+                  width={136}
+                  height={217}
+                />
+              </div>
+            }
+          />
         </div>
       </div>
       <div className="sticky bottom-5 px-5">
@@ -44,6 +74,7 @@ const LandingPage = () => {
           color="primary"
           text="로그인하고 절약 챌린지 참여하기"
           rounded
+          onClick={handleClick}
         />
       </div>
     </>

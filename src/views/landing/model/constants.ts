91d12dc0aeb2425c6d6dab6c344a5c8ecccd4f7b
Kopite22 <nulee1000@gmail.com>
2025-09-19 14:06:07 +0900

    ✨ - 상수 분리

---
+++ src/views/landing/model/constants.ts
@@ -0,0 +1,16 @@
+export const LANDING_SECTION_CONTENT = {
+  others: {
+    title: "매번 돈 아끼려 했지만,\n늘 어려웠죠?",
+    description:
+      "다른 사람들과 함께 절약 미션에 도전하며\n돈을 아끼는 습관을 만들어보세요.",
+  },
+  together: {
+    title: "다 같이 도전하고\n돈 아끼는 습관 챌린지",
+    description:
+      "3일 챌린지부터 7일, 10일 챌린지까지!\n작은 단계부터 차근차근 도전할 수 있어요.",
+  },
+  now: {
+    title: "나도 할 수 있는 돈 아끼기,\n지금 같이 도전해봐요!",
+    description: "챌린지를 통해 절약한 돈을 보며\n동기부여를 얻어요!",
+  },
+};
--- src/views/landing/ui/LandingPage.tsx
+++ src/views/landing/ui/LandingPage.tsx
@@ -1,33 +1,50 @@
 import { Button } from "@/shared/ui/button";
-import { Input } from "@/shared/ui/input";
-import { Body1 } from "@/shared/ui/typography/Body";
-import { Title2 } from "@/shared/ui/typography/Title";
 
+import { LANDING_SECTION_CONTENT } from "../model/constants";
+import ChallengeCard from "./ChallengeCard";
+import SectionLayout from "./SectionLayout";
+import Title from "./Title";
 import Toast from "./Toast";
 
 const LandingPage = () => {
   return (
     <>
-      <div className="h-full" style={{ border: "1px solid red" }}>
+      <div className="pb-10">
         <div className="flex-1">
           <Toast />
 
-          <div className="mt-[33px] flex flex-col gap-y-1">
-            <Title2 className="text-center whitespace-pre-line">{`지금, 돈 아끼는 챌린지\n시작하기!`}</Title2>
-            <Body1 className="bg-linear-to-r from-[#4287FF] to-[#4CA0AA] bg-clip-text text-center font-semibold text-transparent">
-              이미 12,338명이 절약 중
-            </Body1>
+          <Title />
+          <div className="flex flex-col gap-y-5">
+            <SectionLayout
+              title={LANDING_SECTION_CONTENT.others.title}
+              description={LANDING_SECTION_CONTENT.others.description}
+              content={
+                <div className="scrollbar-hide flex gap-x-3 overflow-x-scroll px-4">
+                  <ChallengeCard />
+                  <ChallengeCard />
+                  <ChallengeCard />
+                </div>
+              }
+            />
+            <SectionLayout
+              title={LANDING_SECTION_CONTENT.together.title}
+              description={LANDING_SECTION_CONTENT.together.description}
+            />
+            <SectionLayout
+              title={LANDING_SECTION_CONTENT.now.title}
+              description={LANDING_SECTION_CONTENT.now.description}
+            />
           </div>
         </div>
-        {/* <div className="sticky bottom-5 px-5">
-          <Button
-            variant="solid"
-            size="fullWidth"
-            color="primary"
-            text="로그인하고 절약 챌린지 참여하기"
-            rounded
-          />
-        </div> */}
+      </div>
+      <div className="sticky bottom-5 px-5">
+        <Button
+          variant="solid"
+          size="fullWidth"
+          color="primary"
+          text="로그인하고 절약 챌린지 참여하기"
+          rounded
+        />
       </div>
     </>
   );

8adc191b3df2552edb2a56a1294be10ecefc4a9e
Kopite22 <nulee1000@gmail.com>
2025-10-08 14:54:42 +0900

    ✨ - route 설정 추가

--- src/shared/constants/routeConfig.ts
+++ src/shared/constants/routeConfig.ts
@@ -14,9 +14,17 @@ export const ROUTE_CONFIG: Record<RoutePathValue, RouteConfig> = {
   "/landing": {
     showHeader: true,
     showShareButton: true,
-    backgroundColor: "white",
+    backgroundColor: "common100",
   },
   "/login": {
+    showHeader: true,
+    backgroundColor: "login-background",
+  },
+  "/login/kakao": {
+    showHeader: true,
+    backgroundColor: "login-background",
+  },
+  "/signUp": {
     showHeader: true,
     backgroundColor: "login-background",
   },
@@ -26,4 +34,6 @@ export const ROUTE_PATH = {
   HOME: "/",
   LANDING: "/landing",
   LOGIN: "/login",
+  LOGIN_KAKAO: "/login/kakao",
+  SIGN_UP: "/signUp",
 };

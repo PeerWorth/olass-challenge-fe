b66fd44062b31053192b09156bd3bc7548148465
Kopite22 <nulee1000@gmail.com>
2025-10-08 14:57:59 +0900

    ✨ - 카카오 로그인 추가

---
+++ src/views/login/AuthPage.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+import { authQueries } from "@/shared/api/queryFactory/auth";
+import { ROUTE_PATH } from "@/shared/constants";
+import { Loader } from "@/shared/ui/loader";
+import { PortalOverlay } from "@/shared/ui/portalOverlay";
+
+const AuthPage = () => {
+  const queryString = useSearchParams();
+  const router = useRouter();
+  const idToken = queryString.get("code");
+
+  // const { mutate } = useMutation(
+  //   authQueries.postKakao({
+  //     onSuccess: (data) => {
+  //       console.log(data);
+  //     },
+  //     onError: (error) => {
+  //       console.log(error);
+  //     },
+  //   }),
+  // );
+
+  useEffect(() => {
+    if (idToken) {
+      // mutate({ idToken });
+      router.push(ROUTE_PATH.SIGN_UP);
+    }
+  }, [idToken]);
+
+  return (
+    <>
+      <Loader />
+    </>
+  );
+};
+
+export default AuthPage;
